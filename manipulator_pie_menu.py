0fd4ed682a13f866b5f1b578fa7dfea548fee375
HawkX3D <hawkxcg@gmail.com>
2024-12-11 19:45:05 +0300

    Добавил поддержку горячих клавиш F1/F2/F3 и обновил Pie Menu для управления гизмами
    - Добавлен новый функционал:
      - Горячая клавиша `F1` для активации Location Gizmo.
      - Горячая клавиша `F2` для активации Scale Gizmo.
      - Горячая клавиша `F3` для активации Rotation Gizmo.
    
    - Обновления Pie Menu:
      - Горячая клавиша `Alt+Space` по-прежнему вызывает Pie Menu с управлением гизмами.
      - Добавлена возможность переключения между режимами гизмов напрямую через горячие клавиши.
    
    - Улучшено удобство работы с гизмами в 3D пространстве за счет прямого доступа к основным функциям.

--- manipulator_pie_menu.py
+++ manipulator_pie_menu.py
@@ -1,10 +1,10 @@
 bl_info = {
-    "name": "Manipulator Pie Menu",
+    "name": "Manipulator Pie Menu with F1/F2/F3 Hotkeys",
     "author": "Your Name",
-    "version": (1, 6),
+    "version": (1, 10),
     "blender": (3, 6, 0),
-    "location": "Hotkey (Alt+Space)",
-    "description": "Pie Menu for toggling manipulators (location, rotation, scale, gizmo)",
+    "location": "Hotkey (F1/F2/F3) and Alt+Space for Pie Menu",
+    "description": "Pie Menu for toggling manipulators and F1/F2/F3 for gizmo modes",
     "warning": "",
     "wiki_url": "",
     "category": "3D View",
@@ -90,20 +90,36 @@ def register():
     bpy.utils.register_class(WM_OT_ToggleExclusiveGizmo)
     bpy.utils.register_class(VIEW3D_MT_ManipulatorPieMenu)
 
-    # Добавляем горячую клавишу Alt+Space
+    # Добавляем горячие клавиши для 3D View (во всех режимах)
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name="3D View", space_type='VIEW_3D')
+
+    # Alt+Space для вызова Pie Menu
     kmi = km.keymap_items.new("wm.call_menu_pie", type='SPACE', value='PRESS', alt=True)
     kmi.properties.name = "VIEW3D_MT_manipulator_pie_menu"
-    addon_keymaps.append((km, kmi))
 
-    print("Pie menu 'Manipulator Pie' registered with hotkey Alt+Space.")
+    # F1 для Location
+    kmi1 = km.keymap_items.new("wm.toggle_exclusive_gizmo", type='F1', value='PRESS')
+    kmi1.properties.gizmo = "translate"
+
+    # F2 для Scale
+    kmi2 = km.keymap_items.new("wm.toggle_exclusive_gizmo", type='F2', value='PRESS')
+    kmi2.properties.gizmo = "scale"
+
+    # F3 для Rotation
+    kmi3 = km.keymap_items.new("wm.toggle_exclusive_gizmo", type='F3', value='PRESS')
+    kmi3.properties.gizmo = "rotate"
+
+    addon_keymaps.extend([kmi, kmi1, kmi2, kmi3])
+
+    print("Pie menu 'Manipulator Pie' registered with hotkey Alt+Space and F1/F2/F3 for gizmos.")
 
 
 def unregister():
     for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
     addon_keymaps.clear()
+
     bpy.utils.unregister_class(WM_OT_ToggleExclusiveGizmo)
     bpy.utils.unregister_class(VIEW3D_MT_ManipulatorPieMenu)
 
